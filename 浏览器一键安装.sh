499cca1951ada2cd56abf3669117159a7d7695b8
lehongphong08 <118195102+lehongphong08@users.noreply.github.com>
2023-04-22 20:58:22 +0700

    Add files via upload

---
+++ 浏览器一键安装.sh
@@ -0,0 +1,13 @@
+chmod 0777 *
+# ./cc.sh
+red=$(tput setaf 1);
+white=$(tput setaf 7);
+gren=$(tput setaf 2);
+
+apt update -y; apt upgrade -y; apt install sudo -y;curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt-get update -y && sudo apt-get install yarn -y; sudo apt install nodejs -y; sudo apt install chromium-browser -y;
+sudo apt -y install gcc g++ make;sudo apt -y install htop vnstat;sudo apt-get install -y unzip;sudo apt -y install screen;sudo apt -y install unrar;npm i await-timeout;npm i console-log-level;npm i events; npm i request;npm i chalk; npm i hpagent; npm i got; npm i crypto; npm i cheerio; npm i url; npm i axios; npm i https; npm i request-curl;npm i user-agents; npm i uuid; npm i tmp-promise; sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget; sudo apt-get install -y libgbm-dev;npm i puppeteer-extra puppeteer-extra-plugin-recaptcha puppeteer-extra-plugin-stealth;npm i http http2 crypto tls; npm i randomstring; npm i minimist; npm i cli-color; npm i axios;npm i await-timeout; npm i axios; npm i chalk; npm i cheerio; npm i cli-color; npm i console-log-level; npm i crypto; npm i events; npm i got; npm i hpagent; npm i http; npm i http2; npm i https; npm i minimist;npm i randomstring;npm i request;npm i request-curl;npm i sleep-promise;npm i tls;npm i tmp-promise;npm i url;npm i user-agents;npm i uuid;npm i fake-useragent;npm i colors;npm i request-promise;npm i playwright;npm i playwright-extra;npm i tough-cookie; npm i ua-parser-js; npm i ua-parser-js;npm i gradient-string;npm i hcaptcha-solver;
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update -y; sudo apt install google-chrome-stable -y; wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb; sudo apt install ./google-chrome-stable_current_amd64.deb;
+echo -e "\n${red}@CKDDOS--浏览器一键安装--重新启动中 Reboot Server Required..\n${white}";
